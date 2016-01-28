8445ebb5b49ced5340e47246ee0b67a1cc7e5414
Jaime Victoria <jaime.victoria.90@gmail.com>
2016-01-28 11:17:32 -0600

    Contacts: Added show action
    - Created resources routing for contacts.
    - Added contacts#show action with basic specs.

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe ContactsController do
       expect(response).to render_template(:index)
     end
   end
+
+  describe 'GET show' do
+    let(:contact) { FactoryGirl.create(:contact) }
+
+    before { get :show, id: contact }
+
+    it 'should return the requested contact' do
+      expect(assigns(:contact)).to eq(contact)
+    end
+
+    it 'should render the show template' do
+      expect(response).to render_template(:show)
+    end
+  end
 end
+
